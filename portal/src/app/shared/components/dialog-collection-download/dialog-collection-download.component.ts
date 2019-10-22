67cb25883e13f3862a9070762ed58142a0976a21
Rodrigo Mariano <rodmariano13@gmail.com>
2019-10-22 12:11:10 -0300

    Created download collection function

--- portal/src/app/shared/components/dialog-collection-download/dialog-collection-download.component.ts
+++ portal/src/app/shared/components/dialog-collection-download/dialog-collection-download.component.ts
@@ -1,5 +1,54 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import * as moment from 'moment';
+
+
+function downloadFile(filename: string, data: string, type = 'text/plain;charset=utf-8'): void {
+  // Source: https://stackoverflow.com/a/33542499
+  // this function works with any browser
+
+  // create a blob that contains the 'data'
+  let blob = new Blob([ data ], { type: type });
+
+  if (window.navigator.msSaveOrOpenBlob) {
+    window.navigator.msSaveBlob(blob, filename);
+  } else {
+    // create a new element 'a' to put on the DOM and download the file
+    let element = window.document.createElement('a');
+
+    // add the URL and the filename to the 'element'
+    element.href = window.URL.createObjectURL(blob);
+    element.download = filename;
+
+    // not show element on the screen
+    element.style.display = 'none';
+
+    // add 'element' on the DOM
+    document.body.appendChild(element);
+    // click on it, in order to download the file
+    element.click();
+    // remove it from DOM
+    document.body.removeChild(element);
+  }
+}
+
+function downloadFile2(filename: string, data: string, type = 'text/plain;charset=utf-8'): void {
+  // Source: https://stackoverflow.com/a/18197341
+
+  // create a new element 'a' to put on the DOM and download the file
+  let element = document.createElement('a');
+
+  // default 'data' is equal to 'data:text/plain;charset=utf-8,'
+  element.setAttribute('href', `data:${type},` + encodeURIComponent(data));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+
+  // add 'element' on the DOM, click on it and remove it from DOM
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
 
 
 @Component({
@@ -39,10 +88,10 @@ export class DialogCollectionDownloadComponent implements OnInit {
     this.dialogRef.close();
   }
 
-  private getAssetsToDownload (assetsCheckboxes: object[]): object[] {
+  private getAssetsLinksToDownload (assetsCheckboxes: object[]): string[] {
     // get the assets to download
 
-    let assets_to_download = [];
+    let links_to_download = [];
 
     assetsCheckboxes.forEach((asset: object) => {
       // if this asset was checked, then...
@@ -55,27 +104,32 @@ export class DialogCollectionDownloadComponent implements OnInit {
           // get the assets array memory reference
           let assets = feature['assets'];
 
-          // select just the assets that are inside 'assets' object
+          // select just the links of assets that are inside 'assets' object
           if (asset_name in assets) {
-            assets_to_download.push(assets[asset_name]);
+            links_to_download.push(assets[asset_name].href);
           }
         });
       }
     });
 
-    return assets_to_download;
+    return links_to_download;
   }
 
   public downloadCollection(): void {
-    console.log('\n\ndownloadAssets()');
-    console.log('this.collection_name: ', this.collection_name);
-    console.log('this.features: ', this.features);
-    console.log('this.assetsCheckboxes: ', this.assetsCheckboxes);
+    // get the assets links to download
+    let links_to_download: string[] = this.getAssetsLinksToDownload(this.assetsCheckboxes);
 
-    // get the assets to download
-    let assets_to_download = this.getAssetsToDownload (this.assetsCheckboxes);
+    // join the links in just one string separate by '\n' (print each link on one line)
+    let joined_links_to_download: string = links_to_download.join('\n');
 
-    console.log('assets_to_download: ', assets_to_download);
+    // get 'now' moment (e.g. 2019_10_22_11_59_12_am)
+    let now = moment().format('YYYY_MM_DD_h_mm_ss_a');
+
+    // create a file name based on 'now' moment
+    let filename = `${now}_${this.collection_name}_collection.txt`;
+
+    // download a file with the assets links
+    downloadFile(filename, joined_links_to_download);
   }
 
   public getKeysFromObject(object: Object): Array<string> {
