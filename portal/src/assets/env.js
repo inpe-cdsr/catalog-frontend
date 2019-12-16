3bfc0c5d5995ae5e26c2b0b2cd678679864d0fe0
Beto Noronha <beto_noronha@live.com>
2019-12-16 13:38:59 -0300

    update env.js

--- portal/src/assets/env.js
+++ portal/src/assets/env.js
@@ -2,13 +2,13 @@
 
     window.__env = window.__env || {};
 
-    window.__env.urlGeoServer = 'http://cbers1.dpi.inpe.br:8089/geoserver';
+    window.__env.urlGeoServer = 'http://localhost:8089/geoserver';
 
     window.__env.urlViaCEP = 'http://viacep.com.br/ws';
 
-    window.__env.urlStacCompose = 'http://cbers1.dpi.inpe.br:8089/stac-compose';
+    window.__env.urlStacCompose = 'http://localhost:8089/stac-compose';
 
-    window.__env.urlDGIBack = 'http://cbers1.dpi.inpe.br:8089/api';
+    window.__env.urlDGIBack = 'http://localhost:8089/api';
 
     window.__env.grids = 'States:grid_ibge_states:states; CBERS4:grid_cbers4_mux:grids; LandSat-8:grid_landsat_tm_amsul:grids; Sentinel-2:grid_sentinel_mgrs:grids';
 
