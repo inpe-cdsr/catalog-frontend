6ec30ecfca7b560f104d5b8e717ab779d4304ea4
Rodrigo M. Mariano <rodmariano13@gmail.com>
2020-01-08 16:04:33 -0200

    Links to be tested were added

--- portal/src/assets/env.js
+++ portal/src/assets/env.js
@@ -3,12 +3,15 @@
     window.__env = window.__env || {};
 
     window.__env.urlGeoServer = 'http://localhost:8089/geoserver';
+    // window.__env.urlGeoServer = 'http://cdsr.dpi.inpe.br/geoserver';
 
     window.__env.urlViaCEP = 'http://viacep.com.br/ws';
 
     window.__env.urlStacCompose = 'http://localhost:8089/stac-compose';
+    // window.__env.urlStacCompose = 'http://cdsr.dpi.inpe.br/stac-compose';
 
     window.__env.urlDGIBack = 'http://localhost:8089/api';
+    // window.__env.urlDGIBack = 'http://cdsr.dpi.inpe.br/api';
 
     window.__env.grids = 'States:grid_ibge_states:states; CBERS4:grid_cbers4_mux:grids; LandSat-8:grid_landsat_tm_amsul:grids; Sentinel-2:grid_sentinel_mgrs:grids';
 
